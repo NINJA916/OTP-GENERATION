d1607d29933c5b6dfa77f6d8f5187b0b23d9a68c
Meenakshisundareswaran Ravichandran <jbharathchandar2000@gmail.com>
2021-03-11 13:23:19 +0530

    commit on main

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const Config = {
+    
+    apiKey: "AIzaSyCeOPQtVCERMwFj5KPtz35_lcw1_RRGjQk",
+    authDomain: "otp-generation-49349.firebaseapp.com",
+    projectId: "otp-generation-49349",
+    storageBucket: "otp-generation-49349.appspot.com",
+    messagingSenderId: "1051259752304",
+    appId: "1:1051259752304:web:52e63f0b2e27c319bd49b5"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(Config);
+  
+  export default firebase
